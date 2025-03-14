8d7703c9e8fadabb4b78e779016bb9c58443b056
Asd22 Entertainment <158327205+neoapps-dev@users.noreply.github.com>
2025-03-14 16:51:49 +0300

    Added comp to register components

--- holo/index.js
+++ holo/index.js
@@ -95,7 +95,7 @@ export class Holo {
                =======                                       
                 +=                                                                                                                                                                                                                                                           
 `);
-    
+    window.comp = registerComponent;
     return instance;
   }
 
